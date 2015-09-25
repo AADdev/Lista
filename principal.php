b2a324ad7490e5580de5ca3bd630baa6bd565513
deivakov <deivoz14@gmail.com>
2015-09-25 08:55:01 +0000

    gran avance

--- principal.php
+++ principal.php
@@ -17,19 +17,16 @@ $(document).ready(function() {
 	
 	 $("#linea1").click(function(){
         $("#crearlista").slideToggle("linear");
-         $("#sub2").fadeOut('slow/400/fast');
-          $("#sub3").fadeOut('slow/400/fast');
+       
          	
     });
 	 $("#linea2").click(function(){
         $("#compartirlista").slideToggle("linear");
-         $("#sub1").fadeOut('slow/400/fast');
-          $("#sub3").fadeOut('slow/400/fast');
+        
     });
 	 $("#linea3").click(function(){
-        $("#sub3").slideToggle("linear");
-         $("#sub1").fadeOut('slow/400/fast');
-          $("#sub2").fadeOut('slow/400/fast');
+        $(".narchivadas").slideToggle("linear");
+        $(".sarchivadas").slideToggle("linear");
     });
 
 });	
@@ -101,6 +98,8 @@ $(document).ready(function() {
 	   
 	   $result = mysql_query("SELECT * FROM lista where id_lista = '".$row3[2]."'", $conexion);
 	   
+	  echo "<div class='narchivadas'>";
+	   
 		while ($row = mysql_fetch_array($result)) {
 				echo "<div class='CSSTableGenerator'>";
 			    echo "<table border='0' class='Tlistas'>";
@@ -125,7 +124,48 @@ $(document).ready(function() {
 			    echo "</div>";
 			    echo "</br>";
 			}
+			
+		 echo "</div><!-- narchivadas -->";
 	   }
+	   
+	   
+	   //archivadas-----------------------
+	     $result4 = mysql_query("select * from usuario_lista where id_usuario = '".$iduser."'", $conexion );
+	    while ($row3 = mysql_fetch_array($result4)) {
+	   
+	   
+	   $result = mysql_query("SELECT * FROM lista where id_lista = '".$row3[2]."'", $conexion);
+	   
+	  echo "<div class='sarchivadas' hidden>";
+	   
+		while ($row = mysql_fetch_array($result)) {
+				echo "<div class='CSSTableGenerator'>";
+			    echo "<table border='0' class='Tlistas'>";
+			    echo "<tr>";
+    			echo "<form action='php/borrarl.php' method='post'>";
+    			echo "<input type='text' value='$row[0]' name='idl' hidden>";
+			    echo "<th>$row[1] ✔ </th><th><input type='submit' value='✘' class='btn'></th>";
+			    echo "</form>";
+			    echo "</tr>";
+			    $result2 = mysql_query("SELECT * FROM tarea WHERE id_lista= '$row[0]' and archivada = 1", $conexion);
+			    $X = 1;
+			    while ($row2 = mysql_fetch_array($result2)) {
+    			    echo "<tr>";
+    			    echo "<form action='php/anadirt.php' method='post'>";
+    			    echo "<input type='text' value='$row2[0]' name='idt' hidden>";
+    			    echo "<td>Tarea Nº".$X.": ".$row2[1]."</td><td><input class='btn' type='submit' value='desarchivar'></td>";
+    			    echo "</form>";
+    			    echo "</tr>";
+    			    $X++;
+			    }
+			    echo "</table>";
+			    echo "</div>";
+			    echo "</br>";
+			}
+			
+		 echo "</div><!-- sarchivadas -->";
+	   }
+	
 	
 	
 	
